70b4a285a6462144bd8832a94a0a28d82eb9d46f
lowkey109 <mumfordben65@gmail.com>
2026-05-02 10:54:02 +0000

    Rollback broken PolyEdge visual overlay render

--- client/src/pages/AdminPolyEdgeAetherforge.tsx
+++ client/src/pages/AdminPolyEdgeAetherforge.tsx
@@ -1,22 +1,5 @@
-import PolyEdgeAetherforgeCockpit from "./polyedge/PolyEdgeAetherforgeCockpit";
-import NexoraMicrofishPanel from "@/components/polyedge/NexoraMicrofishPanel";
-import { NexoraVisualSystems } from "@/components/polyedge/NexoraVisualSystems";
-import { NexoraVisualSystemsB } from "@/components/polyedge/NexoraVisualSystemsB";
+import PolyEdgeReferenceOnePage from "./polyedge/PolyEdgeReferenceOnePage";
 
 export default function AdminPolyEdgeAetherforge() {
-  return (
-    <div className="relative min-h-screen bg-black">
-      <PolyEdgeAetherforgeCockpit mode="admin" />
-
-      <div className="fixed bottom-4 left-[170px] right-4 z-50 max-h-[420px] overflow-y-auto rounded-2xl border border-cyan-300/30 bg-black/95 p-3 shadow-[0_0_40px_rgba(34,211,238,0.25)]">
-        <NexoraMicrofishPanel />
-        <div className="mt-4">
-          <NexoraVisualSystems />
-        </div>
-        <div className="mt-4">
-          <NexoraVisualSystemsB />
-        </div>
-      </div>
-    </div>
-  );
+  return <PolyEdgeReferenceOnePage />;
 }
